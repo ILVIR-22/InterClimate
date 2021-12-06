543570b87fb75decc9939ddb5f91894356536b88
Il'vir_Galikeev <gal.ilvir@yandex.ru>
2021-12-06 21:04:31 +0500

    5

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$('.slider-container').slick({
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // arrows: false,
+    autoplaySpeed: 4000,
+    dots: true,
+    dotsClass: 'slider-dots',  
+  });
